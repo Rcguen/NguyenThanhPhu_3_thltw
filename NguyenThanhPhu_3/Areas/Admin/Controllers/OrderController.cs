8d2ffb6c03f8820dc15483224fe472d4e73b852d
Ritchi <Phu.nt2710@gmail.com>
2025-03-25 13:37:03 +0700

    q2eqe

--- NguyenThanhPhu_3/Areas/Admin/Controllers/OrderController.cs
+++ NguyenThanhPhu_3/Areas/Admin/Controllers/OrderController.cs
@@ -1,25 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NguyenThanhPhu_3.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace NguyenThanhPhu_3.Areas.Admin.Controllers
+[Authorize(Roles = "Customer")]
+public class OrderController : Controller
 {
-    [Area("Admin")]
-    public class OrderController : Controller
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public OrderController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Details(int id)
-        {
-            // Lấy thông tin đơn hàng theo ID
-            return View();
-        }
-
-        public IActionResult Delete(int id)
-        {
-            // Xử lý xóa đơn hàng
-            return RedirectToAction("Index");
-        }
+        _context = context;
+        _userManager = userManager;
     }
+
+    public async Task<IActionResult> MyOrderItems()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null) return RedirectToAction("Login", "Account");
+
+        var orderItems = await _context.OrderDetails
+            .Include(od => od.Product)
+            .Include(od => od.Order) // Lấy thông tin đơn hàng
+            .Where(od => od.Order.UserId == user.Id)
+            .OrderByDescending(od => od.Order.OrderDate) // Sắp xếp theo ngày đặt
+            .ToListAsync();
+
+        return View(orderItems);
+    }
+
 }
